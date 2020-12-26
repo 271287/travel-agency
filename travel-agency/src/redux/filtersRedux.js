7e9510f74a3e4c8e40dc73b4651c04a5c130e5d2
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-12-26 23:12:04 +0100

    Add filtering

--- travel-agency/src/redux/filtersRedux.js
+++ travel-agency/src/redux/filtersRedux.js
@@ -1,6 +1,6 @@
 /* SELECTORS */
 
-export const getAllFilters = ({filters}) => filters;
+export const getAllFilters = ({ filters }) => filters;
 
 /* ACTIONS */
 
@@ -8,15 +8,19 @@ export const getAllFilters = ({filters}) => filters;
 const reducerName = 'filters';
 const createActionName = name => `app/${reducerName}/${name}`;
 
-// action types
+// action types TODO - add other action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG_TO_FILTER = createActionName('ADD_TAG_TO_FILTER');
+export const REMOVE_TAG_TO_FILTER = createActionName('REMOVE_TAG_TO_FILTER');
 
-// action creators
+// action creators TODO - add other action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const AddTagToFilter = payload => ({ payload, type: ADD_TAG_TO_FILTER });
+export const RemoveTagToFilter = payload => ({ payload, type: REMOVE_TAG_TO_FILTER });
 
-// reducer
+// reducer handle other action types
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case CHANGE_PHRASE:
@@ -24,7 +28,29 @@ export default function reducer(statePart = [], action = {}) {
         ...statePart,
         searchPhrase: action.payload,
       };
-    // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          [action.payload.type]: action.payload.value,
+        },
+      };
+    case ADD_TAG_TO_FILTER:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags,
+          action.payload.tag,
+        ],
+      };
+    case REMOVE_TAG_TO_FILTER:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags.filter(tag => tag != action.payload.tag),
+        ],
+      };
     default:
       return statePart;
   }
