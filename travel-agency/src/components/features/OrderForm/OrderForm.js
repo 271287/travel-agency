3abbe91c197dc2804faf9b5c34fb456b7e58b744
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-12-29 20:52:19 +0100

    Add OrderOption and other components

--- travel-agency/src/components/features/OrderForm/OrderForm.js
+++ travel-agency/src/components/features/OrderForm/OrderForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import OrderOption from '../OrderOption/OrderOption';
+import pricing from '../../../data/pricing.json';
 
-const OrderForm =(props) => (
+const OrderForm = (props) => (
 
   <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} />
+      </Col>)}
     <Col xs={12}>
-      <OrderSummary cost={props.tripCost} options={props.options}/>
+      <OrderSummary cost={props.tripCost} options={props.options} />
     </Col>
   </Row>
 
