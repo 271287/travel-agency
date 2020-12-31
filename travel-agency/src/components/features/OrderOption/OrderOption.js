4e57ff402c1140cbf8df1c6d2eaaf3e17d96f12d
Natalia Szymiczek <nat.szymiczek@gmail.com>
2020-12-31 23:09:27 +0100

    OrderOptionDate and OrderOptionText

--- travel-agency/src/components/features/OrderOption/OrderOption.js
+++ travel-agency/src/components/features/OrderOption/OrderOption.js
@@ -6,12 +6,16 @@ import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
 import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionText from './OrderOptionText';
+import OrderOptionDate from './OrderOptionDate';
 
 const optionTypes = {
   dropdown: OrderOptionDropdown,
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
+  text: OrderOptionText,
+  date: OrderOptionDate,
 };
 
 const OrderOption = ({ name, type, id, setOrderOption, ...otherProps }) => {
@@ -31,7 +35,7 @@ const OrderOption = ({ name, type, id, setOrderOption, ...otherProps }) => {
   }
 };
 
-OrderOption.propTypes = {
+OrderOption.PropTypes = {
   title: PropTypes.string,
 };
 
